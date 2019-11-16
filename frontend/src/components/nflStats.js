f1d6ae4f8eefb3c17d993b355ebef324d3d272b0
Rohit <moza.rohit@gmail.com>
2019-11-16 16:16:18 -0500

    Added player search functionality

--- frontend/src/components/nflStats.js
+++ frontend/src/components/nflStats.js
@@ -1,13 +1,72 @@
 import React, { useState, useEffect } from 'react'
-import { Table } from 'antd'
+import { Table, Input, Button, Icon } from 'antd';
 import 'antd/dist/antd.css'
 
+const NflStats = () => {
+
+  const [statsList, setStatsList] = useState([])
+  const [searchText, setSearchText] = useState('')
+
+  useEffect(() => {
+  fetch("http://localhost:5000/api/nfl_stats")
+     .then(response => response.json())
+     .then(data => setStatsList(data));
+  }, [])
+
+  
+  const handleSearch = (selectedKeys, confirm) => {
+    confirm()
+    setSearchText(selectedKeys[0])
+  }
+
+  const handleReset = clearFilters => {
+    clearFilters()
+    setSearchText('')
+  }
+
+  let getColumnSearchProps = dataIndex => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters }) => (
+      <div style={{ padding: 8 }}>
+        <Input
+          ref={node => {
+            let searchInput = node;
+          }}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={e => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys, confirm)}
+          style={{ width: 188, marginBottom: 8, display: 'block' }}
+        />
+        <Button
+          type="primary"
+          onClick={() => handleSearch(selectedKeys, confirm)}
+          icon="search"
+          size="small"
+          style={{ width: 90, marginRight: 8 }}
+        >
+          Search
+        </Button>
+        <Button onClick={() => handleReset(clearFilters)} size="small" style={{ width: 90 }}>
+          Reset
+        </Button>
+      </div>
+    ),
+    filterIcon: filtered => (
+      <Icon type="search" style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes(value.toLowerCase()),
+})
 
 const columns = [
   {
     title: "Player Name",
     dataIndex: "player",
-    key: "player"
+    key: "player",
+    ...getColumnSearchProps('player')
   }, 
   {
     title: "Team",
@@ -81,16 +140,6 @@ const columns = [
     key: "fum"
   }
 ]
-
-const NflStats = () => {
-
-  const [statsList, setStatsList] = useState([])
-
-  useEffect(() => {
-  fetch("http://localhost:5000/api/nfl_stats")
-     .then(response => response.json())
-     .then(data => setStatsList(data));
-  }, [])
   
   return(
     <div>
